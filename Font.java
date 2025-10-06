166cba7f5a58c0412be57436dcbf0e80edbdbf0f
greywolfcode <229827496+greywolfcode@users.noreply.github.com>
2025-10-06 15:39:07 -0400

    Added space and more letters

--- Font.java
+++ Font.java
@@ -3,13 +3,14 @@ import java.util.HashMap;
 
 public class Font 
 {
-    //create hashtable of charachtes for font
-    
+    //create hashtable of charachters for font
     HashMap <String, String[]> font = new HashMap<String, String[]>();
-    
+    //init function- add charachter arrays to font hahsmap
     public Font()
     {
-        //add font values
+        //create and add font values
+        String[] space = {"   ", "   ", "   ", "   ", "   "}
+        this.font.put(" ", space)
         String[] a = {"███", "█ █", "███", "█ █", "█ █"};
         this.font.put("a", a);
         String[] b = {"██ ", "█ █", "██ ", "█ █", "██ "};
@@ -25,7 +26,25 @@ public class Font
         String[] g = {" ██", "█  ", "█ █", "█ █", "██ "};
         this.font.put("g", g);
         String[] h = {"█ █", "█ █", "███", "█ █", "█ █"};
-        this.font.put("h", h)
+        this.font.put("h", h);
+        String[] i = {"███", " █ ", " █ ", " █ ", "███"};
+        this.font.put("i", i);
+        String[] j = {"  █", "  █", "  █", "█ █", "███"};
+        this.font.put("j", j);
+        String[] k = {"█ █", "██ ", "█ ", "██ ", "█ █"};
+        this.font.put("k", k);
+        String[] l = {"█  ", "█  ", "█  ", "█  ", "███"};
+        this.font.put("l", l);
+        String[] m = {"█ █", "███", "█ █", "█ █", "█ █"};
+        this.font.put("m", m);
+        String[] n = {"███", "█ █", "█ █", "█ █", "█ █"};
+        this.font.put("n", n);
+        String[] o = {"███", "█ █", "█ █", "█ █", "███"};
+        this.font.put("o", o);
+        String[] p = {"███", "█ █", "███", "█  ", "█  "};
+        this.font.put("p", p);
+        String[] q = {"███", "█ █", "█ █", " ███", " ██"};
+        this.font.put("q", q);
     }
     //method to print text to screen
     public void print(String str)
